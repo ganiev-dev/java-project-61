67248dcf99556dc9b3ebc24c5cae13c0d2f94cac
rustam ganiev <psy12@yandex.ru>
2025-03-19 19:10:54 +0500

    fix bin/brain-prime fail1

--- app/src/main/java/hexlet/code/games/Prime.java
+++ app/src/main/java/hexlet/code/games/Prime.java
@@ -4,20 +4,23 @@ import hexlet.code.Utils;
 
 public class Prime {
     public static void start() {
-        final int minGenVal = 1;
         final int maxGenVal = 100;
 
         //Create arr with questions and right answers
         String[] questionsAndRightAnswers = new String[Engine.ROUND_COUNTER * 2];
         for (int i = 0; i < questionsAndRightAnswers.length; i += 2) {
-            int questionNum = Utils.generateRandomNumber(minGenVal, maxGenVal);
+            int questionNum = Utils.generateRandomNumber(maxGenVal);
             String rightAnswer = "yes";
 
-            for (int j = 2; j <= Math.sqrt(questionNum); j++) {
-                if (questionNum < 2 || questionNum % j == 0) {
-                    rightAnswer = "no";
-                    break;
+            if (!(questionNum < 2)) {
+                for (int j = 2; j * j <= questionNum; j++) {
+                    if (questionNum % j == 0) {
+                        rightAnswer = "no";
+                        break;
+                    }
                 }
+            } else {
+                rightAnswer = "no";
             }
 
             //Question
